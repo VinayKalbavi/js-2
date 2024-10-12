23d5cadabd62296a1bf700f3d8f04f5c2cee4316
VinayKalbavi <vinaykalbhavi32@gmail.com>
2024-10-12 08:16:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14553
+let accountEmail = "vinay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Margao"
+
+//accountId = 2
+accountEmail = "rashu@gmail.com"
+accountPassword = "12345"
+accountCity = "Margao"
+
+ console.log(accountEmail);
+ 
